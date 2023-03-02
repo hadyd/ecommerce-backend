a14ffb5b736efd6227ea5e65bd2da4888c3efe3f
Hadyd <hadyd.arl@gmail.com>
2023-03-02 17:49:39 +0700

    Create Login & Register API with JWT

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ import express from 'express';
 import FileUpload from 'express-fileupload';
 import cors from 'cors';
 import ProductRoute from './routes/ProductRoute.js';
+import UsersRoute from './routes/UserRoute.js';
+import db from './config/Database.js';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(ProductRoute);
+app.use(UsersRoute);
+
+try {
+  await db.authenticate();
+  console.log('Database Connected...');
+} catch (error) {
+  console.error(error);
+}
 
 app.listen(5000, () => console.log('Server Up and Running...'));
--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -6,13 +6,14 @@ import {
   updateProduct,
   deleteProduct,
 } from '../controllers/ProductController.js';
+import { verifyToken } from '../middleware/VerifyToken.js';
 
 const router = express.Router();
 
-router.get('/products', getProducts);
-router.get('/products/:id', getProductById);
-router.post('/products', saveProduct);
-router.patch('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get('/products', verifyToken, getProducts);
+router.get('/products/:id', verifyToken, getProductById);
+router.post('/products', verifyToken, saveProduct);
+router.patch('/products/:id', verifyToken, updateProduct);
+router.delete('/products/:id', verifyToken, deleteProduct);
 
 export default router;
